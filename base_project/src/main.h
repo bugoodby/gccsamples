39992e61a0fc520ba6cb160fbeb7f211a8661793
bugoodby <bugoodby@gmail.com>
2022-07-16 22:51:20 +0900

    add base_project

---
+++ base_project/src/main.h
@@ -0,0 +1,27 @@
+#ifndef __MAIN_H__
+#define __MAIN_H__
+
+
+/*----------------------------------------*/
+/* Module Name & Version                  */
+/*----------------------------------------*/
+#define MODULENAME_STRING	"base_project"
+#define VERSION_STRING		"0.01"
+
+
+/*----------------------------------------*/
+/* OPTION Structure                       */
+/*----------------------------------------*/
+
+typedef struct _GLOBALPROP {
+	int argc;					/* number of input files */
+	char** argv;				/* pointer to a file name array */
+} GLOBALPROP;
+
+
+extern GLOBALPROP g_gprop;
+inline GLOBALPROP* prop() { return &g_gprop; }
+
+
+
+#endif /* __MAIN_H__ */
